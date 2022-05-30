8e31698595799115e7c23bf718b530bb3565420e
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-30 22:02:32 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  with_options presence: true do
+    validates :nickname
+    validates :profile
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
