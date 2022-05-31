d8ff19c46db3a8a87689c703181a1d81b1c75720
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-31 11:45:23 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :cook_name
+    validates :recipe
+    validates :material
+    validates :country
+    validates :memory
+    validates :image
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
   
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
