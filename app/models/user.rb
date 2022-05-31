2209d983eacb47e6aeca2e333144ee61308c20be
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-31 15:08:49 +0900

    bundle exec rubocop -a実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  
+
   has_many :items
 
   devise :database_authenticatable, :registerable,
