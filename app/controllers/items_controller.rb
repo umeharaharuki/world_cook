e96ed4e87fcb30e3c558e2c794b3692d257f1ea1
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-31 12:01:22 +0900

    コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:cook_name, :recipe, :material, :country, :memory, :image).merge(user_id: current_user.id)
+  end
 end
