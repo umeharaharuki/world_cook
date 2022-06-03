11fb46ef40a847b3770eee95969f29fadf60538a
umeharaharuki <jasper1116lopez@icloud.com>
2022-06-03 16:34:51 +0900

    ルーティングとビュー編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
   resources :comments, only: :create
+  collection do
+    get 'search'
+  end
   end
   resources :users, only: :show
 end
