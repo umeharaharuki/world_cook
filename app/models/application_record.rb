1ea2d8374de47250da1c09c5477ef8dc9e84e8d6
umeharaharuki <jasper1116lopez@icloud.com>
2022-06-08 20:29:59 +0900

    bundle exec rubocop -a実行

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-    def previous
-    Item.where("id < ?", self.id).order("id DESC").first
-    end
-    def next
-    Item.where("id > ?", self.id).order("id ASC").first
-    end
+  def previous
+    Item.where('id < ?', id).order('id DESC').first
+  end
+
+  def next
+    Item.where('id > ?', id).order('id ASC').first
+  end
 end
